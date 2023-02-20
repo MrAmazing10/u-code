152b4a30d999262d51438e86038ff7f70173c1e8
Sanchit <sanchitrrk@gmail.com>
2023-02-20 11:29:21 +0530

    added comments for easy understanding

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 import discord
 
 class MyClient(discord.Client):
-    async def on_ready(self):
+    async def on_ready(self): # this bot is ready to take new requests
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
+    async def on_message(self, message): # this listents to the events sent by the server (discord server)
         print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
